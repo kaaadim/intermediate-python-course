cc692f03e6bef1b67f716057608199288b069c94
kaaadim <kaaadim@users.noreply.github.com>
2020-05-12 21:17:28 +0200

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,12 +1,20 @@
 def main():
   import random
  
-  dice_rolls=2
+  dice_rolls=int(input('How many rolls baby?'))
+  dice_sides=int(input('How many sides would you like in a dice?'))
   dice_sum=0
   
   for i in range(0,dice_rolls):
-   roll=random.randint(1,6)
-   print(f'You rolled a {roll}')
+   roll=random.randint(1,dice_sides)
+   if roll==1:
+    print(f'You rolled a {roll}! CRITICAL FAIL')
+   
+   elif roll==dice_sides:
+   	print(f'You rolled a {roll}! CRITICAL SUCESS')
+  
+   else:
+    print(f'You rolled a {roll}')
    dice_sum=dice_sum+roll
   
   print(f'Sum of dices is {dice_sum}')
